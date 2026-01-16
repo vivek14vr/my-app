3585522e1f67583ae59498490baaeb4e41d486f9
vivek14vr <vivek14.vr@gmail.com>
2026-01-16 18:04:19 +0530

    feat: initialize React application with create-react-app setup
    - Add package.json with dependencies and scripts for React app
    - Create public files: favicon.ico, index.html, logo192.png, logo512.png, manifest.json, robots.txt
    - Add basic styles in App.css and index.css
    - Implement main App component and a Header component
    - Set up testing with App.test.js and jest-dom in setupTests.js
    - Include performance reporting with reportWebVitals.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
